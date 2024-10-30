ac8911c5325d13e01a40b5c810229ca359dc6200
MrJuanPabloTheCreator <juanpablocerdasilva@gmail.com>
2024-10-30 15:15:37 -0600

    Landing page section 1 implementation and redesign

--- src/pages/landing/landing.jsx
+++ src/pages/landing/landing.jsx
@@ -1,17 +1,36 @@
 import React, { useState } from 'react';
+
 import SignIn from '../signIn/signIn';
+import styles from "./landing.module.css"
 
 const Landing = () => {
   const [showSignIn, setShowSignIn] = useState(false);
 
   return (
-    <div>
+    <div style={{ flex: 1 }}>
       {showSignIn ? (
         <SignIn setShowSignIn={setShowSignIn}/>
       ) : (
-        <div>
-          <h1>Landing</h1>
-          <button onClick={() => setShowSignIn(true)}>Sign In</button>
+        <div className={styles.landingContainer}>
+          <section>
+            <nav className={styles.navbar}> 
+              <span>JPChat</span>
+              <button>
+                Login
+              </button>
+            </nav>
+            <div className={styles.landingContent}>
+              <div>
+                <h1>Stay Connected, Anytime, Anywhere</h1>
+                <p>ChatPro allows you to message your contacts in real-time, enjoy secure conversations, and interact with our intelligent chatbot to stay on top of your tasks.</p>
+                <button onClick={() => setShowSignIn(true)}>Get Started</button>
+              </div>
+              <img src='../../../assets/webSocketAppIphones.png' alt='Demo Image' height={500}/>
+            </div>
+          </section>
+          <section>
+            Details
+          </section>
         </div>
       )}
     </div>
